3d8feadf7344989de3ce2d69caccc73c81d30f3a
majm <majm@ushareit.com>
2020-08-28 14:55:50 +0800

    [序列化与反序列化 测试]

--- src/test/java/com/mjm/AppTest.java
+++ src/test/java/com/mjm/AppTest.java
@@ -7,14 +7,38 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        System.out.println(c.equals(a + b));
+        System.out.println(g == (a + b));
+        System.out.println(g.equals(a + b));
+
+        System.out.println("================");
+
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1 == str1.intern());
+
+        String str2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str2 == str2.intern());
+
+        String str3 = new StringBuilder("ja").append("vb").toString();
+        System.out.println(str3 == str3.intern());
+
     }
 }
--- src/test/java/com/mjm/SerializableTest.java
+++ src/test/java/com/mjm/SerializableTest.java
@@ -1,8 +1,18 @@
 package com.mjm;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 /**
+ * 1. 单一对象,无继承关系:若想实现序列化与反序列化,则必须实现序列化接口,否则报异常:NotSerializableException
+ * 2. 对象间有继承关系,但无引用关系,若想实现序列化与反序列化,则父类必须实现序列化接口或提供无参构造函数,否则报invalidClassException
+ * 3. 对象间有继承关系,并且有引用关系,若想实现序列化与反序列化,则父类必须实现序列化接口
+ * <p>
+ * <p>
  * Created by majunmin on 2018/11/12.
  */
 public class SerializableTest {
@@ -10,20 +20,25 @@ public class SerializableTest {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("data.out"));
         oos.writeObject(new Sub("mkj", true));
+//        oos.writeObject(new Sub());
 
+        System.out.println(" ========== ");
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream("data.out"));
         Super sub = (Super) ois.readObject();
         System.out.println(sub);
+
+//        sub = (Super) ois.readObject();
+//        System.out.println(sub);
     }
 }
 
-class Super implements Serializable{
+class Super implements Serializable {
 
     private String name;
 
-    public Super() {
-        System.out.println("super construct");
-    }
+//    public Super() {
+//        System.out.println("super construct");
+//    }
 
     public Super(String name) {
         this.name = name;
@@ -38,13 +53,14 @@ class Super implements Serializable{
     }
 }
 
-class Sub extends Super  implements Serializable {
+class Sub extends Super implements Serializable {
 
     private static final long serialVersionUID = 3846752112809249244L;
 
     private boolean fuckable;
 
-    public Sub(){
+    public Sub() {
+        super("test");
         System.out.println("sub construct");
     }
 
@@ -63,6 +79,6 @@ class Sub extends Super  implements Serializable {
     }
 
     public String toString() {
-        return getName()+getFuckable();
+        return getName() + "-" + getFuckable();
     }
 }
